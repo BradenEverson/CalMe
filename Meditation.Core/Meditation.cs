a15a56595c6bea463a321bf1aa82e0a13113e363
Braden Everson <bradeneverson@gmail.com>
2020-07-04 00:58:04 -0500

    Created basic class for meditations, will add more context later

---
+++ Meditation.Core/Meditation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meditation.Core
+{
+    public enum meditationTypes
+    {
+        breathing,
+        yoga,
+        auditory
+    }
+    public class Meditation
+    {
+        public string name { get; set; }
+        public string content { get; set; }
+        public meditationTypes meditationType { get; set; }
+        public bool wasLiked { get; set; }
+    }
+}
